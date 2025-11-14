9d25c75038748534399c8a486e67724ed013501e
nicyousant <90989304+nicyousant@users.noreply.github.com>
2025-11-14 10:50:20 -0500

    added model

---
+++ backend/models/Todo.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const todoSchema = mongoose.Schema({
+    text: { type: String, require: true },
+    completed: { type: Boolean, default: false }
+})
+
+// if you haven't created the collection, first argument will create it
+// if you have the collection already, specify the exact name of it here as the third argument
+// Capital T because it is a class
+const Todo = mongoose.model('todo', todoSchema)
+
+export default Todo
