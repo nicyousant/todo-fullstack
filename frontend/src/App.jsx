5e9109b713334d364c10f0153607e030980e700b
nicyousant <90989304+nicyousant@users.noreply.github.com>
2025-11-14 11:27:16 -0500

    basic output for our list

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,44 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 function App() {
 
+  const [todos, setTodos] = useState([{ text: "Lunch", completed: false, _id: 1 }])
+
   useEffect(() => {
-    async function test() {
-      const response = await fetch('http://localhost:8080')
-      const result = await response.json()
-      console.log(result)
+    async function getTodos() {
+      const response = await fetch('http://localhost:8080/todos')
+      const data = await response.json()
+      console.log(data)
+      // setTodos(data)
     }
-    test()
+    getTodos()
   }, [])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    console.log('handleSubmit')
+  }
 
   return (
     <>
-      
+      <h1>Todos</h1>
+      <form onSubmit={handleSubmit}>
+        <input required={true}/>
+        <button>Submit</button>
+      </form>
+      <ul>
+        {/* in React, map turns array of objects into elements */}
+        {todos.map((todo) => 
+          <li key={todo._id}>
+            <input 
+              type="checkbox" 
+              checked={todo.completed}
+              onChange={() => {}}
+            />
+            {todo.text}
+          </li>
+        ) }
+      </ul>
     </>
   )
 }
